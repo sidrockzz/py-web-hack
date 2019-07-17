b8fdc6377768ca39f29fbee28de32e7b0b9327d6
Siddharth Naidu <siddu1999@gmail.com>
2019-07-17 22:29:22 +0530

    Added php programs

---
+++ PHP/first.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title> My First php program </title>
+ </head>
+ <body>
+  <?php
+   echo "Im Siddharth";
+  ?>
+ </body>
+</html>
---
+++ PHP/second.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+ <title> My second php program </title>
+ </head>
+ <body>
+ <?php
+  echo "<h1>Im a lumberjack </h1>";
+  echo "<h2> Im from the sea</h1";
+?>
+</body>
+</html>
+
