77c704c1fdff7181278c77cac3105e792b316853
Siddharth <marshtomp.1999@hotmail.com>
2020-04-22 19:31:08 +0530

    Added isitnewyear? web app

--- Flask/application.py
+++ Flask/application.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+#imports date time
+import datetime
 #app variable represents this is the current __name__ web application stored
 app = Flask(__name__)
 #route is part of url where we want to go.Under the route if we write any function we it will execute that function
 @app.route("/")
 def index():
-#renders the html page from the template and passing headline as the parameter
- headline = "Hello, World!"
- return render_template("index.html", headline=headline)
+#web app regarding whether it is new year or not
+ now = datetime.datetime.now()
+ new_year = now.month == 1 and now.day == 1
+ return render_template("index.html",new_year=new_year,now=now)
