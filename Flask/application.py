7d8b86ae3250926d320f2fc1532612a1b2a6a6bb
Siddharth <marshtomp.1999@hotmail.com>
2020-04-22 19:04:34 +0530

    Added templates and render_template

--- Flask/application.py
+++ Flask/application.py
@@ -1,13 +1,8 @@
-from flask import Flask,
+from flask import Flask, render_template
 #app variable represents this is the current __name__ web application stored
 app = Flask(__name__)
 #route is part of url where we want to go.Under the route if we write any function we it will execute that function
 @app.route("/")
 def index():
- return "Hello World!!"
-#creates our own hello with the given name on the link
-@app.route("/<string:name>")
-def hello(name):
- name = name.capitalize()
-#can add html tags
- return f"<h1>Hello, {name}!</h1>"
+#renders the html page from the template
+ return render_template("index.html")
