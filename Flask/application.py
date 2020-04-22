34a1071b8f0112df160bb20ad2edff14271770c9
Siddharth <marshtomp.1999@hotmail.com>
2020-04-22 23:12:01 +0530

    Note taking application

--- Flask/application.py
+++ Flask/application.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request
-#imports date time
-import datetime
+from flask import Flask, render_template, request, session
+from flask_session.__init__ import Session
 #app variable represents this is the current __name__ web application stored
 app = Flask(__name__)
 #route is part of url where we want to go.Under the route if we write any function we it will execute that function
-@app.route("/")
+app.config["SESSION_PERMANENT"]= False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+#note taking applications
+notes =[]
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
- return render_template("index.html")
-#submitting the data to hello function for POST whereas GET is for getting the data
-@app.route("/hello", methods=['GET', 'POST'])
-def hello():
- if request.method == "GET":
-  return "<h1>Please submit the form instead</h1>"
- else:
-  name = request.form.get("name")
-  return render_template("hello.html",name=name)
+ if request.method == "POST":
+  note = request.form.get("note")
+  notes.append(note)
+ 
+
+ return render_template("index.html",notes=notes)
