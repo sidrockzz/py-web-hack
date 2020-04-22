d836aaa2fb835a4480315b3e549afe1f1dfc21cb
Siddharth <marshtomp.1999@hotmail.com>
2020-04-22 20:28:51 +0530

    Looping condition in flask using jinja2

--- Flask/application.py
+++ Flask/application.py
@@ -6,7 +6,5 @@ app = Flask(__name__)
 #route is part of url where we want to go.Under the route if we write any function we it will execute that function
 @app.route("/")
 def index():
-#web app regarding whether it is new year or not
- now = datetime.datetime.now()
- new_year = now.month == 1 and now.day == 1
- return render_template("index.html",new_year=new_year,now=now)
+ names = ["Alice", "Bob", "Charlie"]
+ return render_template("index.html",names = names)
